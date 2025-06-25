dd1354003a0dddd5578b0fde041f848782aa37db
interbr <deepmonitor@t-online.de>
2025-06-26 01:52:19 +0200

    Minor cleanup

--- utils/security.php
+++
@@ -1,9 +0,0 @@
-<?php
-function generateKey($input) {
-    return hash('sha256', implode('-', $input));
-}
-
-function unlockMessage($key) {
-    $vault = json_decode(file_get_contents(__DIR__ . '/../data/vault.json'), true);
-    return $vault[$key] ?? null;
-}
